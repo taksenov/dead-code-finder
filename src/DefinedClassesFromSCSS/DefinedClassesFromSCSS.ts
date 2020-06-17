56f5ebd05f897d7fe8484a6d459babfd2b3e9b11
taksenov <taksenov@gmail.com>
2020-06-17 20:11:14 +0500

    DCF-12: feat: new return structure for defineSCSSClasses

--- src/DefinedClassesFromSCSS/DefinedClassesFromSCSS.ts
+++ src/DefinedClassesFromSCSS/DefinedClassesFromSCSS.ts
@@ -2,20 +2,36 @@ import * as fs from 'fs';
 
 import scssParse from 'postcss-scss/lib/scss-parse';
 
+interface ISelectors {
+  selector: string;
+  start: IPosition;
+  end: IPosition;
+  file: string;
+  parentType: string;
+}
+
+interface IPosition {
+  line: number;
+  column: number;
+}
+
 /**
  * Собрирает все SCSS классы, из указанного файла, в массив
  *
  * @param {string} filepath
- * @returns {Array of strings}
+ * @returns {Array of interface ISelectors}
  */
-const definedClassesFromSCSS: (f: string) => string[] = (filepath: string) => {
+const definedClassesFromSCSS: (f: string) => ISelectors[] = (
+  filepath: string,
+) => {
   const sourceCode = fs.readFileSync(filepath, 'utf-8');
   const AST = scssParse(sourceCode);
 
-  let selectors: string[] = [];
+  let selectors: ISelectors[] = [];
 
   AST.walkRules(/^\D/, (rule: any) => {
-    const { parent } = rule;
+    const { parent, source } = rule;
+    const { start, end } = source;
     const { type: parentType } = parent;
 
     let { selector } = rule;
@@ -23,8 +39,12 @@ const definedClassesFromSCSS: (f: string) => string[] = (filepath: string) => {
     if (parentType === 'root' || parentType === 'atrule') {
       selector = selector.replace(/\n/g, '');
       [, selector] = selector.split('.');
+      [selector] = selector.split(' ');
 
-      selectors.push(...selector.split(','));
+      selectors = [
+        ...selectors,
+        { selector, start, end, parentType, file: filepath },
+      ];
     }
 
     selectors = [...new Set(selectors)];
